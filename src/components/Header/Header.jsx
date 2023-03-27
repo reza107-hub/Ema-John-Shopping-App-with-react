4ae4da36dae1e3fd82016d34eaf586497ae68ae2
reza107-hub <rashidaminur104@gmail.com>
2023-03-27 14:43:54 +0600

    header done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg-[#1C2B35;] text-[#FFFFFF]">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">
+            <img src="../../../images/Logo.svg" alt="" />
+          </a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Order</a>
+            </li>
+            <li>
+              <a>Order Review</a>
+            </li>
+            <li>
+              <a>Manage Inventory</a>
+            </li>
+            <li>
+              <a>Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
